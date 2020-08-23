615b01597503e8bd29ca7d6f6b9879cf7bca36aa
Lucas <lbovolini94@gmail.com>
2020-08-23 11:50:18 -0300

    Fixed float to double conversion

--- src/main/java/com/github/lbovolini/mapper/TypeConverter.java
+++ src/main/java/com/github/lbovolini/mapper/TypeConverter.java
@@ -279,6 +279,9 @@ public class TypeConverter {
         if (from.equals(int.class) || from.equals(Integer.class)) {
             return Double.valueOf(String.valueOf(object));
         }
+        if (from.equals(float.class) || from.equals(Float.class)) {
+            return ((Float)object).doubleValue();
+        }
 
         return (double)object;
     }
